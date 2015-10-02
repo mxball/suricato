4b0d223217ab02556b49afc274aacfb76e71c26e
Lucas Takeshi Rodrigues Palma <lucastakeshipalma@gmail.com>
2015-10-02 19:47:27 -0300

    [Config] Query para configurar o banco de dados com as lousas

---
+++ lousas.sql
@@ -0,0 +1,3 @@
+insert into Lousa (nome, endereco) values ("Glad and Sad", "/assets/images/gladSad.png");
+insert into Lousa (nome, endereco) values ("Starfish", "/assets/images/starfish.png");
+insert into Lousa (nome, endereco) values ("Open the Box", "/assets/images/openTheBox.png");
--- readme.md
+++ readme.md
@@ -3,4 +3,5 @@
 ##Spring JPA Setup
 In JPAConfiguration class you must setup your database name, login and password.
   	
-
+Para configurar o banco tem que rodar:
+mysql -uroot suricato < lousa.sql
